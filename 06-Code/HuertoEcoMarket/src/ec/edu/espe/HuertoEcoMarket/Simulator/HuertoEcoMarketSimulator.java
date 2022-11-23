fdda61f898e93566e99efaa2a97d0b28ba8bf0af
David Toapanta <datoapanta16@espe.edu.ec>
2022-11-23 18:02:58 -0500

    HuertoEcoMarketv1.0.3

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/Simulator/HuertoEcoMarketSimulator.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/Simulator/HuertoEcoMarketSimulator.java
@@ -5,5 +5,7 @@ package ec.edu.espe.HuertoEcoMarket.Simulator;
  * @author David Toapanta, Pacstore, DCCO-ESPE
  */
 public class HuertoEcoMarketSimulator {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/ProductList.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/ProductList.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ec.edu.espe.HuertoEcoMarket.model;
-
 /**
  *
  * @author David Toapanta, Pacstore, DCCO-ESPE
  */
 public class ProductList {
+    private String name;
+    private double productPrice;
+    private int idProduct;
+
+    @Override
+    public String toString() {
+        return "ProductList{" + "name=" + name + ", productPrice=" + productPrice + ", idProduct=" + idProduct + '}';
+    }
+    
+
+    public ProductList(String name, double productPrice, int idProduct) {
+        this.name = name;
+        this.productPrice = productPrice;
+        this.idProduct = idProduct;
+    }
+    
+    public ProductList(){
+        this.name = "";
+        this.productPrice = 0.0;
+        this.idProduct = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(int idProduct) {
+        this.idProduct = idProduct;
+    }
+    
     
 }
