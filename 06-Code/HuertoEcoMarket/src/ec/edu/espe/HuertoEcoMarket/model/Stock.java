d466af7d613ccdf21a3dd3d4403c123db4072356
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-11-23 18:05:08 -0500

    ClassDiagrams v 1.1.0

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Stock.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Stock.java
@@ -1,13 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ec.edu.espe.HuertoEcoMarket.model;
 
 /**
  *
  * @author David Toapanta, Pacstore, DCCO-ESPE
+ * @author Alex Trejo, Pacstore, DCCO-ESPE
  */
 public class Stock {
+        
+    
     
 }
