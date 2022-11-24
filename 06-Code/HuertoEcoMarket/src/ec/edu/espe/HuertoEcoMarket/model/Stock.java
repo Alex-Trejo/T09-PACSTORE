a4e97baedd7d91ce3b79461a3b2d522bfc56363c
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-11-23 23:12:45 -0500

    HuertoEcoMarket v 2.0.0

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Stock.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Stock.java
@@ -1,9 +1,26 @@
 package ec.edu.espe.HuertoEcoMarket.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author David Toapanta, Pacstore, DCCO-ESPE
+ * @author Trejo Alex, Pacstore, DCCO-ESPE
  */
 public class Stock {
+    private ArrayList <ProductList>inventory=new ArrayList();
+
+    public ArrayList<ProductList> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<ProductList> inventory) {
+        this.inventory = inventory;
+    }
+    
+        
+    
+    
+    
     
 }
