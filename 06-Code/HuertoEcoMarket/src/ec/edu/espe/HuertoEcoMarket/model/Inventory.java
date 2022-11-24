cadaa1313f2ad95fa36f918082f0c04b046866a8
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-11-24 04:36:14 -0500

    HuertoEcoMarket v 2.3.0

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Inventory.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Inventory.java
@@ -12,27 +12,68 @@ import java.util.ArrayList;
  */
 public class Inventory {
 
-    private ArrayList<RegisterProduct> listProduct = new ArrayList<>();
+    private String name;
+    private int amount;
+    private double unitPrice;
 
-    RegisterProduct registerEntered = new RegisterProduct();
-
-    public ArrayList<RegisterProduct> getListProduct() {
-        return listProduct;
+    public String getName() {
+        return name;
     }
 
-    public void setListProduct(ArrayList<RegisterProduct> listProduct) {
-        this.listProduct = listProduct;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void Remove() {
-        listProduct.remove(registerEntered);
-        System.out.printf("The product %s has been successfully removed. ",registerEntered.getName());
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
     }
 
     @Override
     public String toString() {
-        System.out.println("________________________The list of products entered________________________");
-        return "Inventory{" + "listProduct=" + listProduct + '}';
+        return "Inventory{" + "name=" + name + ", amount=" + amount + ", unitPrice=" + unitPrice + '}';
+    }
+
+    public Inventory() {
+        this.name = "";
+        this.amount = 0;
+        this.unitPrice = 0.0;
+
+    }
+
+    private RegisterProduct listProduct = new RegisterProduct();
+
+    RegisterProduct registerEntered = new RegisterProduct();
+
+    public RegisterProduct getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(RegisterProduct listProduct) {
+        this.listProduct = listProduct;
+    }
+
+    public RegisterProduct getRegisterEntered() {
+        return registerEntered;
+    }
+
+    public void setRegisterEntered() {
+        this.registerEntered = null;
+    }
+
+    public void setRegisterEntered(RegisterProduct registerEntered) {
+        this.registerEntered = registerEntered;
     }
 
 }
