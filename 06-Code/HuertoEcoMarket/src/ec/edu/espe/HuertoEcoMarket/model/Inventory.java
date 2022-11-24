c9187787b66f7d74768253455885ad625c8a09f4
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2022-11-24 00:29:11 -0500

    HuertoEcoMarket v 2.2.5

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Inventory.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/model/Inventory.java
@@ -4,10 +4,35 @@
  */
 package ec.edu.espe.HuertoEcoMarket.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Reishel Tipan, PACSTORE, DCCO-ESPE
  */
 public class Inventory {
-    
+
+    private ArrayList<RegisterProduct> listProduct = new ArrayList<>();
+
+    RegisterProduct registerEntered = new RegisterProduct();
+
+    public ArrayList<RegisterProduct> getListProduct() {
+        return listProduct;
+    }
+
+    public void setListProduct(ArrayList<RegisterProduct> listProduct) {
+        this.listProduct = listProduct;
+    }
+
+    public void Remove() {
+        listProduct.remove(registerEntered);
+        System.out.printf("The product %s has been successfully removed. ",registerEntered.getName());
+    }
+
+    @Override
+    public String toString() {
+        System.out.println("________________________The list of products entered________________________");
+        return "Inventory{" + "listProduct=" + listProduct + '}';
+    }
+
 }
