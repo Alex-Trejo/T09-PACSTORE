db3211991435ce273ce01ca304dcd2b004a14e41
Reishel Dayelin Tipán Ávila <rdtipan1@espe.edu.ec>
2022-11-28 00:53:41 -0500

    V4.1.0 HuertoEcoMarket

--- 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/Simulator/HuertoEcoMarketSimulator.java
+++ 06-Code/HuertoEcoMarket/src/ec/edu/espe/HuertoEcoMarket/Simulator/HuertoEcoMarketSimulator.java
@@ -93,3 +93,99 @@ public class HuertoEcoMarketSimulator {
                                                 System.out.println("Enter again:");
                                                 search = console.nextInt();
                                             }
+                                            
+                                            search -= 1;
+                                            System.out.println("======================================");
+                                            System.out.println("Select:\n(1)Do you want to eliminate stock?\n(2)Do you want to increase stock?\nEnter your answer:");
+                                            answer = console.nextInt();
+
+                                            if (answer == 1) {
+                                                printSelectedProduct(stock, search);
+                                                eliminateQuantity = console.nextInt();
+                                                existingAmount = stock.get(search).getAmount();
+                                                eliminateQuantity = checkQuantityToBeEliminated(eliminateQuantity, existingAmount, console);
+
+                                                amounts = existingAmount - eliminateQuantity;
+
+                                                stock.get(search).setAmount(amounts);
+                                                printModified(stock, search);
+                                                addJson(stock);
+                                                confirmation = 2;
+
+                                            } else if (answer == 2) {
+                                                printProductIncreased(stock, search);
+                                                increaseTheQuantity = console.nextInt();
+                                                existingAmount = stock.get(search).getAmount();
+                                                amounts = existingAmount + increaseTheQuantity;
+
+                                                stock.get(search).setAmount(amounts);
+                                                printModified(stock, search);
+                                                addJson(stock);
+                                                confirmation = 2;
+
+                                            } else {
+                                                System.out.println("The options are from 1 to 2");
+
+                                            }
+
+                                        }
+
+                                    } else {
+                                        System.out.println("No products have been registered yet");
+                                    }
+
+                                }
+                                case 2 -> {
+                                    if (registrationOfIncomingProducts != 0) {
+
+                                        printAllListOfProduct(stock, position);
+
+                                    } else {
+                                        System.out.println("No prodcuts have been registered yet");
+
+                                    }
+                                }
+
+                                case 3 -> {
+
+                                    submenuOptions = 3;
+                                    break;
+                                }
+                                default ->
+                                    System.out.println("The options are from 1 to 3");
+
+                            }
+
+                        }
+
+                    }
+                    case 3 -> {
+                        int itemToBeSold = 0;
+                        int productSell = 0;
+                        int existingAmount = 0;
+                        double UnitPrice;
+                        int amounts;
+                        if (registrationOfIncomingProducts != 0) {
+                            System.out.println("-------------------------Sales registration system-----------------");
+                            System.out.println("=======================================================");
+                            System.out.println("--------List of products in stock:---------");
+                            System.out.println("Name\t\t\t\t  UnitPrice\t\t\t");
+                            for (int i = 0; i < registrationOfIncomingProducts; i++) {
+
+                                System.out.printf("[%d].%s \t\t\t   %.2f\t\t\t\t   \n", (i + 1), stock.get(i).getName(), stock.get(i).getUnitPrice());
+
+                            }
+
+                            System.out.print("Enter the item number of the item to be sold:\t");
+                            itemToBeSold = console.nextInt();
+
+                            while (itemToBeSold <= 0 || itemToBeSold > stock.size()) {
+                                System.out.println("Not registered");
+                                System.out.println("Enter again:");
+                                itemToBeSold = console.nextInt();
+                            }
+                            itemToBeSold -= 1;
+                            System.out.print("Enter the quantity to be sold:\t");
+                            productSell = console.nextInt();//quantity of product sold
+                            existingAmount = stock.get(itemToBeSold).getAmount();
+                            UnitPrice = stock.get(itemToBeSold).getUnitPrice();
