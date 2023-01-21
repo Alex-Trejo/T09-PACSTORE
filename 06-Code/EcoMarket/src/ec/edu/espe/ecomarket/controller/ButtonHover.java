8eada6d30ac208863b80fa638b448bf444c9c7cb
Alex Fernando Trejo Duque <aftrejo@espe.edu.ec>
2023-01-21 06:22:44 -0500

    EcoMarket v 8.9.1

--- 06-Code/EcoMarket/src/ec/edu/espe/ecomarket/controller/ButtonHover.java
+++ 06-Code/EcoMarket/src/ec/edu/espe/ecomarket/controller/ButtonHover.java
@@ -17,21 +17,23 @@ import javax.swing.JButton;
  * @author Carlos Torres, T09_PACSTORE, DCCO-ESPE
  */
 public class ButtonHover {
-    
+
     private static final int GROW_SIZE = 10;
+
     public static void hoverGrow(JButton button) {
         Point point = button.getLocation();
         Dimension size = button.getSize();
         ImageIcon icon = (ImageIcon) button.getIcon();
         Image img = icon.getImage();
-        Image newimg = img.getScaledInstance(size.width + GROW_SIZE, size.height + GROW_SIZE,  java.awt.Image.SCALE_SMOOTH);
+        Image newimg = img.getScaledInstance(size.width + GROW_SIZE, size.height + GROW_SIZE, java.awt.Image.SCALE_SMOOTH);
         ImageIcon newIcon = new ImageIcon(newimg);
         button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                button.setBounds(point.x - GROW_SIZE/2, point.y - GROW_SIZE/2, size.width + GROW_SIZE, size.height + GROW_SIZE);
+                button.setBounds(point.x - GROW_SIZE / 2, point.y - GROW_SIZE / 2, size.width + GROW_SIZE, size.height + GROW_SIZE);
                 button.setIcon(newIcon);
             }
+
             @Override
             public void mouseExited(MouseEvent e) {
                 button.setBounds(point.x, point.y, size.width, size.height);
@@ -39,48 +41,5 @@ public class ButtonHover {
             }
         });
     }
-    
-}
-
-    
-    
-    
-    
-    
-    
-    
-    /*private static final int GROW_SIZE = 10;
-    private static final int MINIMUN_SIZE = 20;
-
-    public static void hoverGrowEntered(JButton button) {
-        Point point = button.getLocation();
-        Dimension size = button.getSize();
-        ImageIcon icon = (ImageIcon) button.getIcon();
-        Image img = icon.getImage();
-        Image newimg = img.getScaledInstance(size.width + GROW_SIZE, size.height + GROW_SIZE,  java.awt.Image.SCALE_SMOOTH);
-        ImageIcon newIcon = new ImageIcon(newimg);
-
-        button.setBounds(point.x - GROW_SIZE / 2, point.y - GROW_SIZE / 2, size.width + GROW_SIZE, size.height + GROW_SIZE);
-        //button.setSize(size.width + GROW_SIZE, size.height + GROW_SIZE);
-        //button.setLocation(point);
-        button.setIcon(newIcon);
-    }
-
-    public static void hoverGrowExit(JButton button) {
-        Point point = button.getLocation();
-        Dimension size = button.getSize();
-        ImageIcon icon = (ImageIcon) button.getIcon();
-        Image img = icon.getImage();
-        Image newimg = img.getScaledInstance(size.width + GROW_SIZE, size.height + GROW_SIZE,  java.awt.Image.SCALE_SMOOTH);
-        ImageIcon newIcon = new ImageIcon(newimg);
-        
-        button.setBounds(point.x + GROW_SIZE / 2, point.y + GROW_SIZE / 2, size.width - GROW_SIZE, size.height - GROW_SIZE);
-        button.setIcon(newIcon);
-
-        //button.setSize(size.width - GROW_SIZE, size.height - GROW_SIZE);
-        //button.setLocation(point);
-    }
 
 }
-*/
-   
