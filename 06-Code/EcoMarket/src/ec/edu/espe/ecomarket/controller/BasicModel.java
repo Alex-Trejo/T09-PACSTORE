c895723bc30b670e4e859a2d48802a78fd0fd48a
David Toapanta <datoapanta16@espe.edu.ec>
2023-03-01 03:16:51 -0500

    V8.10.0

---
+++ 06-Code/EcoMarket/src/ec/edu/espe/ecomarket/controller/BasicModel.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.ecomarket.controller;
+import com.mongodb.client.result.DeleteResult;
+import org.bson.Document;
+/**
+ *
+ * @author David Toapanta, Pacstore, DCCO-ESPE
+ */
+public abstract class BasicModel {
+
+    public abstract void create(Document document)  ;
+    public abstract Document read(String id, String fieldName);
+    public abstract Document read(Document document);
+    public abstract DeleteResult delete(String id, Object idValue);
+    public abstract void update(String id, String idValue, String updateKey, String valueUpdate);
+    public abstract void update(Document query, Document upload);
+
+    
+}
---
+++ 06-Code/EcoMarket/src/ec/edu/espe/ecomarket/controller/Connection.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.ecomarket.controller;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+/**
+ *
+ * @author David Toapanta, Pacstore, DCCO-ESPE
+ */
+public class Connection {
+
+    public static MongoDatabase mongodb = null;
+
+    private Connection() {
+
+    }
+
+    public static MongoDatabase connectionDataBase() {
+
+        if (mongodb == null) {
+            String uri = "mongodb+srv://alextrejo:1402oop@cluster0.ydafxco.mongodb.net/?retryWrites=true&w=majority";
+            String db = "PACSTORE";
+
+            MongoClient mongoClient = MongoClients.create(uri);
+
+            Connection.mongodb = mongoClient.getDatabase(db);
+        }
+
+        return Connection.mongodb;
+
+    }
+}
